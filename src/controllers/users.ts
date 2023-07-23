6fdb8abfe228272ffe271ead229b176feb59f7ad
Phahladira-Moja <phahladira@patala.co.za>
2023-07-23 03:16:18 +0200

    added the update route for users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -9,6 +9,46 @@ export const getAllUsers = async (
     const users = await getUsers();
 
     return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deleteUser = await deleteUserById(id);
+
+    return res.json(deleteUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(403);
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
