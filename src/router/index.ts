74e3eb27727c2d7edd2c70e0ffaf975d50b94a65
Phahladira-Moja <phahladira@patala.co.za>
2023-07-23 02:57:51 +0200

    added the users controller for getting users
    - Added middleware to the project and to the get users route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import users from "./users";
 import authentication from "./authentication";
+
 const router = express.Router();
 
 export default (): express.Router => {
+  users(router);
   authentication(router);
   return router;
 };
